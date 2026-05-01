a651d110f1e2d65ac4a72b3ca2152561576f2052
ericka <emtrevino@gmail.com>
2026-05-01 12:28:38 -0500

    fix datestring rangeerror on fractional epoch seconds

--- tests/specs/service-worker.spec.ts
+++ tests/specs/service-worker.spec.ts
@@ -14,6 +14,7 @@ test('Service worker test: DateString', async ({serviceWorker}) => {
               'string': new DateString('2024-03-10T03:00:00-04:00'),
               'number': new DateString(1777529052.500),
               'date': new Date(1777529052000),
+              'fractionalEpoch': new DateString(1777570208.089697),
             }
 
             testResolve(JSON.parse(JSON.stringify(dates)))
@@ -21,9 +22,10 @@ test('Service worker test: DateString', async ({serviceWorker}) => {
         })
         .then((workerResponse) => {
           expect(workerResponse).toEqual({
-            'date': '2026-04-30T06:04:12.000Z', 
-            'number': '2026-04-30T06:04:12.5Z', 
-            'string': '2024-03-10T07:00:00Z'
+            'date': '2026-04-30T06:04:12.000Z',
+            'number': '2026-04-30T06:04:12.5Z',
+            'string': '2024-03-10T07:00:00Z',
+            'fractionalEpoch': '2026-04-30T17:30:08.089696768Z',
           })
 
           resolve()
